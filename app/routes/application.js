313f0c056979a42dbb137daafd93be406b77a6d5
Henning Glatter-Götz <henning@glatter-gotz.com>
2015-06-23 11:38:47 +0200

    Add missing semicolon

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function () {
-        return this.store.find('setting', 1)
+        return this.store.find('setting', 1);
     }
 });
